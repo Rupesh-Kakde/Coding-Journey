55f8b5ac4bf295cde8e2ae6967d385909ae6b56e
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-04-27 23:29:05 +0530

    Add files via upload

---
+++ C_Programming/DynamicMemoryCalloc.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+     int i = 0;             //Loop Counter
+    int Size = 0;
+    float *marks = NULL;
+
+    printf("Enter no of Elements: \n");
+    scanf("%d",&Size);
+
+    //Dynamic Memory Allocation
+    marks = (float *)calloc(Size , sizeof(float));  
+
+  
+    printf("Enter Your Marks\n");
+
+    //Iteration
+    //        1     2    3
+    for(int i = 0; i<Size; i++)
+    {
+        scanf("%f",&marks[i]);  //4
+    }
+    
+    printf("Entered Marks are: \n");
+    
+     for(int i = 0; i<Size; i++)
+    {
+        printf("%f\n",marks[i]); 
+    }
+   
+
+    free(marks);
+    return 0;
+    
+}
+
+
+
+//for calloc just one change malloc-> calloc 
---
+++ C_Programming/First.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+    int main()
+
+    {
+        printf("Marvellous Infosystems\n");
+        return 0;
+    }
+
+
+    //dir -> show the files
+    //gcc First.c -> create a binary file in PPA folder
+    // cls -> clear screen
+    // gcc First.c -o Myexe -> create Myexe file
+    // ./Myexe.exe -> Program run (MArvellous Infosystems)
+    // gcc First.c -save-temps -o Myexe (Show all files )
+
+
+
+    //parameter.c
+    //parameter.x
+    //
+
+
+
+    
