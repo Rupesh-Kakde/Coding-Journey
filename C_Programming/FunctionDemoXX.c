c509ab6fa17a27215ada76d6c63708dce486bf72
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-05-08 23:35:32 +0530

    Add files via upload

---
+++ C_Programming/FunctionDemoXX.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+void display()
+{
+
+    printf("Jay Ganesh...\n");
+
+
+}
+
+
+int main()
+{
+    display();
+    display();
+    display();
+
+    return 0;
+}  //8,9,10,3,4,5,6,11
