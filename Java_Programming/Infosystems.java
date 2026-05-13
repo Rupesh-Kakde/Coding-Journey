20b5cf225bdf47cb7a33d6b04d9fd3d703331063
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-05-13 23:05:29 +0530

    Add files via upload

---
+++ Java_Programming/Infosystems.java
@@ -0,0 +1,23 @@
+
+package PPA;
+
+public class Infosystems
+{
+    public void gun()
+    {
+        System.out.println("Inside gun of Infosystems");
+    }
+}
+
+
+
+// public class Marvellous 
+// {
+   
+   
+   
+//     public static void main(String[] args) 
+//    {
+    
+//    } 
+// }
