a878a6eebb16309e27f6e1dad7b4dfe453ae5045
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-05-14 22:42:48 +0530

    Add files via upload

---
+++ C_Programming/ShortLong.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 11;
+    short int j = 11;
+    long int k = 11;
+
+    printf("sizeof int : %d\n",sizeof(i));         // 4 
+    printf("size of short int : %d\n",sizeof(j)); // 2
+    printf("size of long int : %lu\n",sizeof(k));  // 8
+
+
+
+
+
+
+   
+    return 0;
+}
