d3f1e18da0245ddd3220872793f77bef19be30df
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-05-17 22:06:48 +0530

    Add files via upload

---
+++ Java_Programming/MultiThreadingLoopJoin.java
@@ -0,0 +1,40 @@
+class Demo extends Thread
+{
+    public void run()
+    {
+        int  i = 0;
+
+        for(i = 1; i<=100; i++)
+        {
+          System.out.println("Inside Run Method of : "+Thread.currentThread().getName()+" : "+ i);
+        }
+        
+    }
+}
+
+
+public class MultiThreadingLoopJoin
+{
+    
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("Main Thread is Running....");
+
+        Demo dobj1 = new Demo();
+        Demo dobj2 = new Demo();
+
+        dobj1.setName("First");
+        dobj2.setName("Second");
+
+        dobj1.start();
+        dobj2.start();
+
+        dobj1.join();
+        dobj2.join();
+
+        System.out.println("End of Main thread");
+        
+        
+    
+    }
+}
---
+++ Java_Programming/MultiThreadingLoopX.java
@@ -0,0 +1,37 @@
+class Demo extends Thread
+{
+    public void run()
+    {
+        int  i = 0;
+
+        for(i = 1; i<=100; i++)
+        {
+          System.out.println("Inside Run Method of : "+Thread.currentThread().getName()+" : "+ i);
+        }
+        
+    }
+}
+
+
+public class MultiThreadingLoopX
+{
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("Main Thread is Running....");
+
+        Demo dobj1 = new Demo();
+        Demo dobj2 = new Demo();
+
+        dobj1.setName("First");
+        dobj2.setName("Second");
+
+        dobj1.start();
+        dobj2.start();
+
+        System.out.println("End of Main thread");
+        
+        
+    
+    }
+}
