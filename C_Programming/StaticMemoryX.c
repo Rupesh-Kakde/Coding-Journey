d4fad016bf7fc9bcb050ac3a87306224030f03d4
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-05-17 22:08:48 +0530

    Add files via upload

---
+++ C_Programming/StaticMemoryX.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    //Static Memory Allocation
+    float marks[5];        //20 bytes
+
+    printf("Enter Your Marks\n");
+
+    //Sequence
+    scanf("%f",&marks[0]); 
+    scanf("%f",&marks[1]);
+    scanf("%f",&marks[2]);
+    scanf("%f",&marks[3]);
+    scanf("%f",&marks[4]);
+
+    printf("Entered Marks are: \n");
+    
+    printf("%f\n",marks[0]);
+    printf("%f\n",marks[1]);
+    printf("%f\n",marks[2]);
+    printf("%f\n",marks[3]);
+    printf("%f\n",marks[4]);
+
+
+
+    return 0;
+    
+}
