6127ab43356ec9f23886d55738afd45d4a9dfa81
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-05-18 21:49:13 +0530

    Add files via upload

---
+++ C_Programming/StringCreate.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    char Arr[6] = {'H','E','L','L','O','\0'};
+    char Brr[] = {'H','E','L','L','O','\0'};
+    char Crr[6] = "Hello";
+
+    printf("%s\n",Arr);
+    printf("%s\n",Brr);
+    printf("%s\n",Crr);
+
+
+
+
+
+
+    return 0;
+
+}
