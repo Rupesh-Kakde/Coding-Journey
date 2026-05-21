8aa8571a1670f00f8e2fe1a0ca2a914cb35fd2ba
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-05-21 23:38:16 +0530

    Add files via upload

---
+++ C_Programming/StringPointer.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[] = "Ganesh";
+    char *ptr = NULL;       //1 byte 
+
+    ptr = str;      //100
+
+    printf("%c\n",*ptr);        //G
+    ptr++;      //101
+
+    printf("%c\n",*ptr);        //a
+    ptr++;        //102
+
+    printf("%c\n",*ptr);       //n
+    ptr++;      //103
+
+
+
+    return 0;
+
+}
