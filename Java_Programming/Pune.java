3afb94cb09ef6348bb7a7e5f037896bd69850a84
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-05-21 23:38:45 +0530

    Add files via upload

---
+++ Java_Programming/Pune.java
@@ -0,0 +1,23 @@
+
+package PPA.LB;
+
+public class Pune
+{
+    public void sun()
+    {
+        System.out.println("Inside sun of Pune");
+    }
+}
+
+
+
+// public class Marvellous 
+// {
+   
+   
+   
+//     public static void main(String[] args) 
+//    {
+    
+//    } 
+// }
