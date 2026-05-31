66fc6ac5ef145e66a6e8c0656412eb858effaf52
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-05-31 22:13:11 +0530

    Add files via upload

---
+++ C++_Programming/Generic.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+template <class T>       //Template Header
+
+T Addition (T A, T B)
+{
+    T Ans;
+    Ans = A+B;
+    return Ans;
+
+}
+
+int main()
+{
+    cout<<Addition(10,11)<<"\n";
+    cout<<Addition(10.3f,11.2f)<<"\n";
+
+    cout<<Addition(10.789,11.2345)<<"\n";
+
+    return 0;
+
+}
