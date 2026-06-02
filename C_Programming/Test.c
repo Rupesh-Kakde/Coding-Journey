fc25b52d58641ea2c186afdc9de215194d302134
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-06-02 23:26:15 +0530

    Add files via upload

---
+++ C_Programming/Test.c
@@ -0,0 +1,25 @@
+// Demonstration of Preprocessor
+
+/*
+    Pre Placement Activity
+    22 April 2026
+*/
+
+#include<stdio.h>
+#define PI 3.14
+
+int main()
+{
+    // Area of Circle
+    float area = 0.0f;
+    
+    area = 10.5f * 10.5f * PI;
+
+    printf("Area of Circle is : %f\n", area);
+    
+
+
+
+
+    return 0;
+}
