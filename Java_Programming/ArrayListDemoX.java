6c937e349c9952827549ef0b649271df8261961b
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-06-09 23:56:46 +0530

    Add files via upload

---
+++ Java_Programming/ArrayListDemoX.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class ArrayListDemoX
+
+{
+   public static void main(String[] args) 
+   {    
+        ArrayList <String> aobj = new ArrayList <String> ();
+
+        
+        aobj.add("C++");
+        aobj.add("Python");
+        aobj.add("Java");
+        
+        System.out.println(aobj);
+
+     
+
+
+   } 
+}
