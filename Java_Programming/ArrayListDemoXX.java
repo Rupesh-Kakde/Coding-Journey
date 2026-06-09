4b0668a72e1a86a399b9572d5f372a7cf9525b92
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-06-09 23:57:09 +0530

    Add files via upload

---
+++ Java_Programming/ArrayListDemoXX.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class ArrayListDemoX
+
+{
+   public static void main(String[] args) 
+   {    
+        ArrayList <Float> aobj = new ArrayList <Float> ();
+
+        
+        aobj.add(90.67f);
+        aobj.add(10.11f);
+        aobj.add(11.67f);
+        
+        System.out.println(aobj);
+
+     
+
+
+   } 
+}
