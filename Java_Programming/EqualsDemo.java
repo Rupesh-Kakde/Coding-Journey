fa841379289ca0f93fd030880d8745c4216fdc16
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-06-16 09:38:35 +0530

    Add files via upload

---
+++ Java_Programming/EqualsDemo.java
@@ -0,0 +1,36 @@
+
+class Marvellous
+{
+  public int i, j, k;
+
+
+  public Marvellous(int A, int B, int C)
+  {
+    this.i = A;
+    this.j = B;
+    this.k = C;
+
+  }
+
+
+}
+
+public class EqualsDemo 
+{
+    public static void main(String[] args) 
+    {
+       Marvellous mobj1 = new Marvellous(11, 21, 51);
+       Marvellous mobj2 = new Marvellous(11, 21, 51);
+
+
+       if (mobj1.equals(mobj2)) 
+       {
+        System.out.println("Objects are identical"); 
+       }
+       else
+       {
+        System.out.println("Objects are different");
+       }
+    }
+    
+}
