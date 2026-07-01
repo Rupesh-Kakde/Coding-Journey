3b691e4d6036fc4fb1215a5a82cc0fa777f3d17c
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-07-01 23:52:56 +0530

    Add files via upload

---
+++ Java_Programming/HashTableDemo.java
@@ -0,0 +1,23 @@
+import java.util.Hashtable;
+
+public class HashTableDemo
+ 
+{
+    public static void main(String[] args)
+     {
+        Hashtable <String, Integer> hobj = new Hashtable <String, Integer> ();
+
+        hobj.put("PPA", 27000);
+        hobj.put("LB", 28000);
+        hobj.put("PYTHON", 30000);
+        hobj.put("LB", 28500);
+
+        System.out.println(hobj.get("PYTHON"));
+        System.out.println(hobj.get("PPA"));
+        System.out.println(hobj.get("LB"));
+
+        System.out.println(hobj);
+
+
+    }
+}
