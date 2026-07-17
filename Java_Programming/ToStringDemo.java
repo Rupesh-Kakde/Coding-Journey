897920255e0ed5cc28421778cc18e35f6f8842b9
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-07-17 23:56:07 +0530

    Add files via upload

---
+++ Java_Programming/ToStringDemo.java
@@ -0,0 +1,17 @@
+class Marvellous
+{
+   
+}
+
+
+
+public class ToStringDemo
+{
+    public static void main(String[] args)
+    {
+       Marvellous mobj = new Marvellous();
+       
+       System.out.println(mobj.toString());
+    }
+    
+}
