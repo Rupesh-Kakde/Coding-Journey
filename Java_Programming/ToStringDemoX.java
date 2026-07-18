cbcd6403558c6ecba2278862ed93e8d62683d053
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-07-18 21:56:42 +0530

    Add files via upload

---
+++ Java_Programming/ToStringDemoX.java
@@ -0,0 +1,20 @@
+class Marvellous
+{
+  public String toString()
+  {
+    return "Marvellous Pune";
+  } 
+}
+
+
+
+public class ToStringDemoX
+{
+    public static void main(String[] args)
+    {
+       Marvellous mobj = new Marvellous();
+       
+       System.out.println(mobj.toString());
+    }
+    
+}
